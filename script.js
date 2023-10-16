7d9b48d0da72aa2ba4a5f00fcf5713029ce4d08f
Lashagurgenidze1989 <lashagurgenidze1989@gmail.com>
2023-10-16 13:12:59 -0600

    why active is null???

---
+++ script.js
@@ -0,0 +1,23 @@
+const allList = document.querySelectorAll(".accordion-list");
+let active = null;
+debugger;
+allList.forEach((item) => {
+  const header = item.querySelector(".accordion-header");
+  const question = item.querySelector(".question");
+  const answer = item.querySelector(".answer");
+  const images = item.querySelector("#icon");
+
+  header.addEventListener("click", () => {
+    answer.style.display = "block";
+    question.style.fontWeight = "bold";
+    images.style.transform = "rotate(180deg)";
+
+    if (active) {
+      active.querySelector(".answer").style.fontWeight = "none";
+      active.querySelector(".question").style.fontWeight = "normal";
+      active.querySelector("#icon").style.transform = "rotate(180deg)";
+    }
+
+    active = header;
+  });
+});
