5c93c22063b6873bcd059307a30443b959e4d119
Lashagurgenidze1989 <lashagurgenidze1989@gmail.com>
2023-10-17 10:09:46 -0600

    add box illustration

--- script.js
+++ script.js
@@ -2,22 +2,21 @@ const allList = document.querySelectorAll(".accordion-list");
 let active = null;
 
 allList.forEach((item) => {
-  const header = item.querySelector(".accordion-header");
   const question = item.querySelector(".question");
   const answer = item.querySelector(".answer");
   const images = item.querySelector("#icon");
 
-  header.addEventListener("click", () => {
+  item.addEventListener("click", () => {
     answer.style.display = "block";
     question.style.fontWeight = "bold";
     images.style.transform = "rotate(180deg)";
 
-    /* if (active) {
-      active.querySelector(".answer").style.fontWeight = "none";
+    if (active) {
+      active.querySelector(".answer").style.display = "none";
       active.querySelector(".question").style.fontWeight = "normal";
-      active.querySelector("#icon").style.transform = "rotate(180deg)";
+      active.querySelector("#icon").style.transform = "rotate(0deg)";
     }
 
-    active = header; */
+    active = item;
   });
 });
